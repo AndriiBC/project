0bcd4c34ebbd69f090ba0673110d45430054a4fd
Andrii <examp>
2022-04-08 22:31:13 +0300

    function ES6

--- scripts.js
+++ scripts.js
@@ -138,7 +138,7 @@ if (4 == 9) {
 
 // 2.11
 
-let num = 50;
+//let num = 50;
 
 //while (num <= 55) {
 //  console.log(num);
@@ -164,4 +164,46 @@ for (let i = 1; i <8; i++) {
   console.log(i);
 }
 
+//    2.13
 
+let num = 20;
+
+function showFirstMessage(text) {
+  console.log(text);
+  num = 10;
+  console.log(num);
+}
+
+showFirstMessage("Hello World");
+console.log(num);
+
+//function calc(a, b) {
+//  return (a + b);
+//}
+
+//console.log(calc(4, 3));
+//console.log(calc(5, 6));
+//console.log(calc(10, 6));
+
+function ret() {
+  let num = 50;
+
+  //
+
+
+  return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function() {
+  console.log("hello");
+};
+
+logger();
+
+const calc = (a, b) => {
+  console.log('1');
+  return a + b;
+} ;
