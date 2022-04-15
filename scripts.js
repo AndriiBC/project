b1c1c74e57ef48cdcbb6ebef5a3a02ddf0040baf
Andrii <examp>
2022-04-15 22:38:15 +0300

    this

--- scripts.js
+++ scripts.js
@@ -270,3 +270,24 @@ name = "Pete";
 
 sayHi();
 
+var ader = {
+  base : 1,
+
+  add : function(a) {
+    var f = v => v + this.base;
+    return f(a);
+  },
+
+  addThruCall: function(a) {
+    var f = v => v + this.base;
+    var b = {
+      base : 2
+    };
+
+    return f.call(b, a);
+  }
+};
+
+console.log(adder.add(1));         
+console.log(adder.addThruCall(1));
+
